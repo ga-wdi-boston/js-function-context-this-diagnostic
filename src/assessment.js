b0aa90f729f48e92f7a6d391d6ba654affd46737
arvindeva <arvindeva@gmail.com>
2015-08-11 09:42:55 -0400

    Finished scope context assessment.

--- src/assessment.js
+++ src/assessment.js
@@ -8,11 +8,11 @@
 //
 // NOTE: Line numbers are annotated line numbers, not editor line numbers.
 
-/*1*/ var alderaan = "Boom";
-/*2*/ var deathStar = function deathStar(){
-/*3*/   var laserQuantity = 1;
-/*4*/   var laserSize = "huge";
-/*5*/   var fire = function fire(){
+/*1*/ var alderaan = { name: 'Alderaan' };
+/*2*/ var deathStar = {
+/*3*/   laserQuantity: 1,
+/*4*/   laserSize: "huge",
+/*5*/   fire: function(){
 /*6*/     // some code
 /*7*/   };
 /*8*/ };
@@ -23,7 +23,7 @@
 //
 //     `true` or `false`
 //
-var q1 = /* your answer here */;
+var q1 = true;
 //
 
 // Which scope(s) may access the variable `laswerQuantity`? Choose one or many
@@ -31,7 +31,7 @@ var q1 = /* your answer here */;
 //
 //     `['alderaan', 'deathStar', 'fire', 'global']`
 //
-var q2 = [/* your answer here */];
+var q2 = ['deathStar', 'fire'];
 //
 
 // Which of the following are defined within the scope of the `fire` function?
@@ -39,14 +39,14 @@ var q2 = [/* your answer here */];
 //
 //     `['alderaan', 'chartreuse', 'frickinLasers', 'laserSize']`
 //
-var q3 = [/* your answer here */];
+var q3 = [];
 //
 
 // Edit line 6 to read `conosole.log(this)`. Insert a line between 7 and 8 that
 // invokes `fire`. If we invoked `deathStar` in a browser environment, what
 // would the execution context of line 6 be?
 //
-var q4 = "/* your answer here */";
+var q4 = "window";
 //
 
 // Edit line 1 to read `var alderaan = { name: 'Alderaan' }`.
@@ -58,5 +58,5 @@ var q4 = "/* your answer here */";
 // the context of `alderaan`.
 //
 var q5 = function fireOnAlderaan () {
-  // your code here
+  deathStar.fire.call(alderaan);
 };
