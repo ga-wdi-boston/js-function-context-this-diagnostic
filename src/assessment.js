de495bc6133e768eaedd99e006bd74b9170c3853
lekova <lina.lekova@gmail.com>
2015-08-11 09:56:56 -0400

    Assessment done

--- src/assessment.js
+++ src/assessment.js
@@ -7,31 +7,33 @@
 // renders previous answers nonsensical.
 //
 // NOTE: Line numbers are annotated line numbers, not editor line numbers.
-
-/*1*/ var alderaan = "Boom";
+/*1*/ var alderaan = { name: 'Alderaan' }
 /*2*/ var deathStar = function deathStar(){
 /*3*/   var laserQuantity = 1;
 /*4*/   var laserSize = "huge";
 /*5*/   var fire = function fire(){
-/*6*/     // some code
+/*6*/     console.log(this);
 /*7*/   };
+        fire();
 /*8*/ };
 
+deathStar();
+
 // Suppose we had declared another function between line 7 and 8. Would that
-// new function have access to the value stored in the variable `laseQuantity`
+// new function have access to the value stored in the variable `laserQuantity`
 // on line 3?
 //
 //     `true` or `false`
 //
-var q1 = /* your answer here */;
+var q1 = true;
 //
 
-// Which scope(s) may access the variable `laswerQuantity`? Choose one or many
+// Which scope(s) may access the variable `laserQuantity`? Choose one or many
 // of the following:
 //
 //     `['alderaan', 'deathStar', 'fire', 'global']`
 //
-var q2 = [/* your answer here */];
+var q2 = ['deathStar', 'fire'];
 //
 
 // Which of the following are defined within the scope of the `fire` function?
@@ -39,14 +41,14 @@ var q2 = [/* your answer here */];
 //
 //     `['alderaan', 'chartreuse', 'frickinLasers', 'laserSize']`
 //
-var q3 = [/* your answer here */];
+var q3 = ['alderaan', 'laserSize'];
 //
 
 // Edit line 6 to read `conosole.log(this)`. Insert a line between 7 and 8 that
 // invokes `fire`. If we invoked `deathStar` in a browser environment, what
 // would the execution context of line 6 be?
 //
-var q4 = "/* your answer here */";
+var q4 = "undefined";
 //
 
 // Edit line 1 to read `var alderaan = { name: 'Alderaan' }`.
@@ -58,5 +60,17 @@ var q4 = "/* your answer here */";
 // the context of `alderaan`.
 //
 var q5 = function fireOnAlderaan () {
-  // your code here
+  deathStar.fire(alderaan);
 };
+
+var deathStar = {
+  name: null,
+  laserQuantity: 1,
+  laserSize: "huge",
+  fire: function (obj) {
+    this.name = obj.name;
+    console.log(this.name);
+  }
+};
+
+q5();
