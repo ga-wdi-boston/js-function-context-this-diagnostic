2acb34d67a968a9670eff6b4febfbcc70d648eff
gmankd <pabloguzman26@yahoo.com>
2015-11-17 09:33:23 -0500

    submitting pull request

--- src/assessment.js
+++ src/assessment.js
@@ -13,17 +13,24 @@
 /*3*/   var laserQuantity = 1;
 /*4*/   var laserSize = "huge";
 /*5*/   var fire = function fire(){
-/*6*/     // some code
+/*6*/   conosole.log(this)
 /*7*/   };
+deathStar();
 /*8*/ };
 
 // Suppose we had declared another function between line 7 and 8. Would that
 // new function have access to the value stored in the variable `laseQuantity`
 // on line 3?
 //
-//     `true` or `false`
-//
-var q1 = /* your answer here */;
+//     `true`
+// This is a scope chain. And it does have access to laseQuantity on line 3.
+
+var q1 = function q1() {
+  something goes here.
+}
+
+};
+
 //
 
 // Which scope(s) may access the variable `laswerQuantity`? Choose one or many
@@ -31,7 +38,7 @@ var q1 = /* your answer here */;
 //
 //     `['alderaan', 'deathStar', 'fire', 'global']`
 //
-var q2 = [/* your answer here */];
+var q2 = [fire];
 //
 
 // Which of the following are defined within the scope of the `fire` function?
@@ -39,14 +46,14 @@ var q2 = [/* your answer here */];
 //
 //     `['alderaan', 'chartreuse', 'frickinLasers', 'laserSize']`
 //
-var q3 = [/* your answer here */];
+var q3 = ['alderaan', 'laserSize'];
 //
 
 // Edit line 6 to read `conosole.log(this)`. Insert a line between 7 and 8 that
 // invokes `fire`. If we invoked `deathStar` in a browser environment, what
 // would the execution context of line 6 be?
 //
-var q4 = "/* your answer here */";
+var q4 =
 //
 
 // Edit line 1 to read `var alderaan = { name: 'Alderaan' }`.
