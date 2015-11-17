0439e508fe3ee7e26e19158b9b2ed4bdf1e0d25e
Ben <benjamin.a.spencer@gmail.com>
2015-11-17 09:27:23 -0500

    response to assessment

--- src/assessment.js
+++ src/assessment.js
@@ -8,14 +8,21 @@
 //
 // NOTE: Line numbers are annotated line numbers, not editor line numbers.
 
-/*1*/ var alderaan = "Boom";
-/*2*/ var deathStar = function deathStar(){
-/*3*/   var laserQuantity = 1;
-/*4*/   var laserSize = "huge";
-/*5*/   var fire = function fire(){
-/*6*/     // some code
-/*7*/   };
-/*8*/ };
+/*1*/ var alderaan = { name: 'Alderaan' };
+/*2*/ var deathStar = {
+        laserQuantity: 1,
+        laserSize: "huge",
+        fire: function () {
+         console.log(this);
+        }
+      }
+// function deathStar(){
+// /*3*/   var laserQuantity = 1;
+// /*4*/   var laserSize = "huge";
+// /*5*/   var fire = function fire(){
+// /*6*/     conosole.log(this);
+// /*7*/   };
+// /*8*/ };
 
 // Suppose we had declared another function between line 7 and 8. Would that
 // new function have access to the value stored in the variable `laseQuantity`
@@ -23,7 +30,7 @@
 //
 //     `true` or `false`
 //
-var q1 = /* your answer here */;
+var q1 = true; //the new function would still be in the scope of deathstar
 //
 
 // Which scope(s) may access the variable `laswerQuantity`? Choose one or many
@@ -31,7 +38,7 @@ var q1 = /* your answer here */;
 //
 //     `['alderaan', 'deathStar', 'fire', 'global']`
 //
-var q2 = [/* your answer here */];
+var q2 = ['deathStar', 'fire'];
 //
 
 // Which of the following are defined within the scope of the `fire` function?
@@ -39,14 +46,14 @@ var q2 = [/* your answer here */];
 //
 //     `['alderaan', 'chartreuse', 'frickinLasers', 'laserSize']`
 //
-var q3 = [/* your answer here */];
+var q3 = ['laserSize'];
 //
 
 // Edit line 6 to read `conosole.log(this)`. Insert a line between 7 and 8 that
 // invokes `fire`. If we invoked `deathStar` in a browser environment, what
 // would the execution context of line 6 be?
 //
-var q4 = "/* your answer here */";
+var q4 = "deathStar.call(fire)"; //if we invoked deathStar in a brower environment, the context would be the window
 //
 
 // Edit line 1 to read `var alderaan = { name: 'Alderaan' }`.
@@ -58,5 +65,6 @@ var q4 = "/* your answer here */";
 // the context of `alderaan`.
 //
 var q5 = function fireOnAlderaan () {
-  // your code here
+  deathStar.fire(alderaan);
 };
+
