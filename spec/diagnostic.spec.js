5e48a2f172e437bf6edb1659b412ca6090bfeb21
racheltstevens <racheltstevens@gmail.com>
2016-11-22 11:34:00 -0500

    Update tests to run in mocha, close #120
    Updated for 015

--- spec/diagnostic.spec.js
+++ spec/diagnostic.spec.js
@@ -1,18 +1,25 @@
 'use strict';
 
-let _ = require('lodash');
-let diagnostic = require('../lib/diagnostic');
+const chai = require('chai');
+const _ = require('lodash');
 
-describe('access', () => {
+const expect = chai.expect;
+
+const diagnostic = require('../lib/diagnostic');
+
+
+describe('access', function () {
   it('references the correct primitive boolean', () => {
-    expect(diagnostic.access).toBe(true);
+    expect(diagnostic.access).to.be.true;
   });
 });
 
 describe('scopes', () => {
   it('is an array containing the correct strings in any order', () => {
+    expect(diagnostic.scopes).not.to.eql([]);
+    expect(diagnostic.scopes.length).to.eql(2);
     let correct = function (answer) {
-      expect(_.includes(['deathStar', 'fire'], answer)).toBe(true);
+      expect(_.includes(['deathStar', 'fire'], answer)).to.be.true;
     };
     diagnostic.scopes.forEach(correct);
   });
@@ -20,6 +27,6 @@ describe('scopes', () => {
 
 describe('fireOnAlderaan', () => {
   it('is called on the correct object', () => {
-    expect(diagnostic.fireOnAlderaan()).toBe('Alderaan');
+    expect(diagnostic.fireOnAlderaan()).to.eql('Alderaan');
   });
 });
